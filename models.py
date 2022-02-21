458e5a9f8e905eff416873e6c5b638636bb7d088
ALLOWAKOU Yao Ferdinand <allowakouferdinand@gmail.com>
2022-02-21 23:46:34 +0100

    mon premier commit pour l'api

---
+++ models.py
@@ -0,0 +1,20 @@
+from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
+
+
+class Todo(models.Model):
+
+    id = fields.IntField(pk=True)
+    title = fields.CharField(max_length=250)
+    descripton = fields.CharField(max_length=250)
+    begin_date = fields.CharField(max_length=250)
+    end_date = fields.CharField(max_length=250)
+    status = fields.IntField()
+
+
+    class PydanticMeta:
+        pass
+
+
+Todo_Pydantic = pydantic_model_creator(Todo, name="Todo")
+TodoIn_Pydantic = pydantic_model_creator(Todo, name="TodoIn", exclude_readonly=True)
